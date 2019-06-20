08f031ea33876ce4e4d705e5245a727df3406337
YasiruUOK <yasirukavinda89@gmail.com>
2019-06-20 19:36:04 +0530

    2019-06-20 7.30pm

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signIn } from '../../store/actions/authActions'
 
 class SignIn extends Component {
   state = {
@@ -12,9 +14,10 @@ class SignIn extends Component {
   }
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signIn(this.state)
   }
   render() {
+    const { authError } = this.props;
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -36,4 +39,15 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn
+const mapStateToProps = (state) => {
+  return{
+    authError: state.auth.authError
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return{
+    signIn: (creds) => dispatch(signIn(creds))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,25 @@
-const initState = {}
+const initState = {
+  authError: null
+}
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch(action.type){
+    case 'LOGIN_ERROR':
+      console.log('login fail')
+      return{
+        ...state,
+        authError: 'Login failed'
+      } 
+    case 'LOGIN_SUCCESS':
+      console.log('login success');
+      return{
+        ...state,
+        authError: null
+      }
+    default:
+      return state;
+  }
+  return state
 };
 
-export default authReducer;
+export default authReducer
